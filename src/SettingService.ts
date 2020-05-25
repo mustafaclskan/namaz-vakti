166d9724412741e857956aa6f2b4dadb11c9eb35
canbax <yusufsaidcanbaz@gmail.com>
2020-05-25 23:24:13 +0300

    start using localStorage

--- src/SettingService.ts
+++ src/SettingService.ts
@@ -1,5 +1,6 @@
 
 export class SettingService {
+  static QUEUE_LIMIT = 3;
 
   static setCSS(theme: string) {
     const file = document.getElementById('theme-link') as HTMLLinkElement;
@@ -13,5 +14,21 @@ export class SettingService {
     } else {
       return [];
     }
+  }
+
+  static addTimesData(key: string, data: any) {
+    localStorage.setItem(key, JSON.stringify(data));
+    const d = localStorage.getItem('timesQueue');
+    if (d != null) {
+      let size = localStorage.getItem('timesQueueSize') ?? 0;
+      size = +size; // convert to number
+      if (size > this.QUEUE_LIMIT) {
+        const idOfFirst = localStorage.getItem('timesQueueFirst');
+        if (idOfFirst != null) {
+          localStorage.removeItem(idOfFirst);
+        }
+      }
+    }
+
   }
 }
