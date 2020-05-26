79902f5e8e6291205c5db890934205c80a4c9936
canbax <yusufsaidcanbaz@gmail.com>
2020-05-26 22:47:30 +0300

    delete primevue, add vuetify started

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import { SettingService } from './SettingService';
 
-import Sidebar from 'primevue/sidebar';
-import Button from 'primevue/button';
-import Dropdown from 'primevue/dropdown';
-import Card from 'primevue/card';
-import Fieldset from 'primevue/fieldset';
-import Panel from 'primevue/panel';
-
-Vue.component('Sidebar', Sidebar);
-Vue.component('Button', Button);
-Vue.component('Dropdown', Dropdown);
-Vue.component('Card', Card);
-Vue.component('Fieldset', Fieldset);
-Vue.component('Panel', Panel);
 
 Vue.config.productionTip = false
 
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
+import vuetify from './plugins/vuetify';
 SettingService.setInitialTheme();
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
