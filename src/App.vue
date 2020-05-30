94e21667a4d54ec18c0e8a15e5dd030ac96d5a11
canbax <yusufsaidcanbaz@gmail.com>
2020-05-30 19:08:19 +0300

    Uı changed with v-list

--- src/App.vue
+++ src/App.vue
@@ -10,68 +10,19 @@
     </v-app-bar>
 
     <v-content>
-      <v-list :flat="true" v-if="currTimes && currTimes[currTimeIdx]" class>
-        <v-list-item-group v-model="currPrayIdx">
-          <v-list-item>
-            <h2 class="normal-font">{{currTimes[currTimeIdx].MiladiTarihUzun}}</h2>
-          </v-list-item>
-          <v-list-item>
-            <h2 class="normal-font">{{currTimes[currTimeIdx].MiladiTarihUzun}}</h2>
-          </v-list-item>
-          <v-list-item>
-            <h2 class="normal-font">{{currTimes[currTimeIdx].MiladiTarihUzun}}</h2>
-          </v-list-item>
-          <v-list-item>
-            <h2 class="normal-font">{{currTimes[currTimeIdx].MiladiTarihUzun}}</h2>
-          </v-list-item>
-          <v-list-item>
-            <h2 class="normal-font">{{currTimes[currTimeIdx].MiladiTarihUzun}}</h2>
-          </v-list-item>
-          <v-list-item>
-            <h2 class="normal-font">{{currTimes[currTimeIdx].MiladiTarihUzun}}</h2>
-          </v-list-item>
-
-          <!-- <v-list-item-content>{{currTimes[currTimeIdx].HicriTarihUzun}}</v-list-item-content>
+      <v-list :flat="true" disabled v-if="currTimes && currTimes[currTimeIdx]" class="txt-center">
+        <v-list-item-group>
+          <v-list-item
+            v-for="(item, i) in timeItems"
+            :key="i"
+            v-bind:class="{ accent: i == currPrayIdx + 2 }"
+          >
             <v-list-item-content>
-              <h1 class="normal-font primary--text">İmsak: {{currTimes[currTimeIdx].Imsak}}</h1>
+              <h1 class="normal-font">{{item.pre}} {{currTimes[currTimeIdx][item.key]}}</h1>
             </v-list-item-content>
-            <v-list-item-content>
-              <h1 class="normal-font">Güneş: {{currTimes[currTimeIdx].Gunes}}</h1>
-            </v-list-item-content>
-            <v-list-item-content></v-list-item-content>
-            <v-list-item-content></v-list-item-content>
-          <v-list-item-content></v-list-item-content>-->
+          </v-list-item>
         </v-list-item-group>
       </v-list>
-      <!-- <v-card v-if="currTimes && currTimes[currTimeIdx]" class="txt-center m-5">
-        <div>
-          <h2 class="normal-font">{{currTimes[currTimeIdx].MiladiTarihUzun}}</h2>
-        </div>
-        <div>{{currTimes[currTimeIdx].HicriTarihUzun}}</div>
-        <v-divider></v-divider>
-        <div color="primary">
-          <h1 class="normal-font primary--text">İmsak: {{currTimes[currTimeIdx].Imsak}}</h1>
-        </div>
-        <div>
-          <h1 class="normal-font">Güneş: {{currTimes[currTimeIdx].Gunes}}</h1>
-        </div>
-        <div>
-          <h1 class="normal-font">Öğle: {{currTimes[currTimeIdx].Ogle}}</h1>
-        </div>
-        <div>
-          <h1 class="normal-font">İkindi: {{currTimes[currTimeIdx].Ikindi}}</h1>
-        </div>
-        <div>
-          <h1 class="normal-font">Akşam: {{currTimes[currTimeIdx].Aksam}}</h1>
-        </div>
-        <div>
-          <h1 class="normal-font">Yatsı: {{currTimes[currTimeIdx].Yatsi}}</h1>
-        </div>
-        <v-divider></v-divider>
-        <div>
-          <h2 class="normal-font">{{currPray}} vaktine kalan süre {{remainingTime}}</h2>
-        </div>
-      </v-card>-->
     </v-content>
   </v-app>
 </template>
@@ -93,9 +44,18 @@ export default class App extends Vue {
   private currTimeIdx = 0;
   private currLoc: string | null = "";
   private currPray = "Akşam";
-  private currPrayIdx = 3;
+  private currPrayIdx = 2;
   private remainingTime = "1:38:21";
-  private timeItems = [""];
+  private timeItems = [
+    { pre: "", key: "MiladiTarihUzun" },
+    { pre: "", key: "HicriTarihUzun" },
+    { pre: "İmsak:", key: "Imsak" },
+    { pre: "Güneş:", key: "Gunes" },
+    { pre: "Öğle:", key: "Ogle" },
+    { pre: "İkindi:", key: "Ikindi" },
+    { pre: "Akşam:", key: "Aksam" },
+    { pre: "Yatsı:", key: "Yatsi" }
+  ];
 
   created() {
     this.currTimes = SettingService.getTimes4CurrentLocation();
