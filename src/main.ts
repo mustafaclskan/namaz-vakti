2a5aaef02c0ee9fa8d4fa52f7a5ae1d14781d42c
canbax <yusufsaidcanbaz@gmail.com>
2020-05-31 16:53:19 +0300

    add i18n using vue cli

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import './registerServiceWorker'
 Vue.config.productionTip = false
 
 import vuetify from './plugins/vuetify';
+import i18n from './i18n'
 
 new Vue({
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
