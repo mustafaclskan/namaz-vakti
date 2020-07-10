e4a93bcbd674ac399e24855ea38ee0a999d6822d
canbax <yusufsaidcanbaz@gmail.com>
2020-07-10 22:37:18 +0300

    start substr translator

---
+++ src/SubstrTranslator.ts
@@ -0,0 +1,28 @@
+
+export class SubstrTranslator {
+  private _months = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran',
+    'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'];
+
+  private _days = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'];
+
+  private _prays = ['İmsak', 'Güneş', 'Öğle', 'İkindi', 'Akşam', 'Yatsı'];
+
+  constructor() {
+
+  }
+
+  public translateDayName(s: string) {
+    for (const d of this._days) {
+      
+    }
+  }
+
+  public translatePrayName(s: string) {
+
+  }
+
+  public translateMonthName(s: string) {
+
+  }
+
+}
