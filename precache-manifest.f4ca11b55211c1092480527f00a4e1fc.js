1e60a76a50d66566db7b337e415f54577bad61e4
canbax <yusufsaidcanbaz@gmail.com>
2020-08-03 22:55:03 +0300

    add dist folder to here

---
+++ precache-manifest.f4ca11b55211c1092480527f00a4e1fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622e7a566b9508c53202",
+    "url": "/namaz-vakti/css/app.d9cc9ca3.css"
+  },
+  {
+    "revision": "1aca8001b4d21446eb7f",
+    "url": "/namaz-vakti/css/chunk-vendors.824aefeb.css"
+  },
+  {
+    "revision": "0de3c6ff8b76465832737d8a1c4ce097",
+    "url": "/namaz-vakti/index.html"
+  },
+  {
+    "revision": "622e7a566b9508c53202",
+    "url": "/namaz-vakti/js/app.e2aa8ae9.js"
+  },
+  {
+    "revision": "1aca8001b4d21446eb7f",
+    "url": "/namaz-vakti/js/chunk-vendors.b4465ce8.js"
+  },
+  {
+    "revision": "1739f89d60d3bc90bf1e774e7f705c08",
+    "url": "/namaz-vakti/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/namaz-vakti/robots.txt"
+  }
+]);
