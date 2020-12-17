136391699473575d5f0f90cb63c860408fd87366
canbax <yusufsaidcanbaz@gmail.com>
2020-12-17 21:47:02 +0300

    #5

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
+  <div class="m5">
     {{ $t("aboutTxt") }}
+    <div>
+      <a href="https://github.com/canbax/namaz-vakti" title="Source Codes">
+        {{ $t("sourceCodes") }}
+      </a>
+    </div>
+    <div>
+      <a href="https://canbax.github.io/" title="Contact">
+        {{ $t("contact") }}
+      </a>
+    </div>
+    <br>
+    <div>
+      Icons made by
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+      from
+      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </div>
   </div>
 </template>
 
