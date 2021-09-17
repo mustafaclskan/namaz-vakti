23415a0d940e890e7c6d3154de3b1cb5d3f2219b
canbax <yusufsaidcanbaz@gmail.com>
2021-09-17 06:02:26 +0300

    add pwa config

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,20 @@ module.exports = {
       enableInSFC: true
     }
   },
+  pwa: {
+    name: "Namaz Vakti",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: `src/registerServiceWorker.ts`,
+      // ...other Workbox options...
+    },
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/namaz-vakti/'
     : '/'
